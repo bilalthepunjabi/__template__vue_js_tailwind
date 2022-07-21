3e4f1b2acfce26ae6db320b48a8cad89e6a53587
Bilal Punjabi <bilalthepunjabi@gmail.com>
2022-07-21 12:30:29 +0000

    --tweaks

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        
+    <div class="grid h-screen place-items-center bg-gradient-to-r from-gray-500 via-gray-700 to-gray-500">
+        <div class="text-white">
+            <h1 class="text-7xl text-center">{{'<Application />'}}</h1>
+            <p class="text-4xl underline decoration-wavy decoration-2 underline-offset-8 whitespace-normal">This will be the start of something great!</p>
+        </div>
     </div>
 </template>
